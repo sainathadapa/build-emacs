eb31f6d52af213509ff1803df7b0c32d07fee088
Sainath Adapa <sainath@findhotel.net>
2018-05-29 11:13:52 +0200

    not using stow

--- build-emacs.sh
+++ build-emacs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Build latest version of Emacs, version management with stow
-# OS: Ubuntu 14.04 LTS and newer
+# OS: Ubuntu 18.04 LTS
 # Toolkit: gtk3
 
 set -eu
 
-readonly version="25.3"
+readonly version="26.1"
 
 # install dependencies
 sudo apt-get -qq update
@@ -30,17 +30,9 @@ fi
 sudo mkdir -p /usr/local/{bin,etc,games,include,lib,libexec,man,sbin,share,src}
 
 # build and install
-sudo mkdir -p /usr/local/stow
 cd emacs-"$version"
 ./configure \
     --with-xft \
     --with-x-toolkit=gtk3
-
 make
-sudo make \
-    install-arch-dep \
-    install-arch-indep \
-    prefix=/usr/local/stow/emacs-"$version"
-
-cd /usr/local/stow
-sudo stow emacs-"$version"
+sudo make install
